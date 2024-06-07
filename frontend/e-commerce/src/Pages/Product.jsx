f4bfc8d041c502e32a19f3aea5a5e83ac12deefc
Raven Alexander <theravenalexander@gmail.com>
2024-06-07 04:19:51 -0600

    cart items functionality

--- frontend/e-commerce/src/Pages/Product.jsx
+++ frontend/e-commerce/src/Pages/Product.jsx
@@ -3,8 +3,9 @@ import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
 import Breadcrumb from '../Components/Breadcrumbs/Breadcrumb';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 
-const Product = (props) => {
+const Product = () => {
     const {all_product}= useContext(ShopContext);
     const {productId} = useParams();
     const product = all_product.find((e)=> e.id === Number(productId))
@@ -12,6 +13,7 @@ const Product = (props) => {
     <div>
       <Breadcrumb product={product} />
       <ProductDisplay product={product} />
+      <DescriptionBox />
     </div>
   )
 }
