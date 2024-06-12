47bc7344fe3e18d6bc223f3229cfdd6784e9d946
Raven Alexander <theravenalexander@gmail.com>
2024-06-12 07:38:08 -0600

    page responsive css

--- frontend/e-commerce/src/Context/ShopContext.jsx
+++ frontend/e-commerce/src/Context/ShopContext.jsx
@@ -31,7 +31,7 @@ const ShopContextProvider = (props) => {
     const addtoCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         // console.log(cartItems); - for testing
-        if(localStorage.getItem('auth-token')) {
+        if(localStorage.getItem('auth-token')) { //if the user is logged in, the rest will happen
             fetch('http://localhost:4000/addtocart', {
                 method: 'POST',
                 headers: {
