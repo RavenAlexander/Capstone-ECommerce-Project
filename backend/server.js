f1c23f310cbfcba4a5dd03a0ee95e97e93a6015e
Raven Alexander <theravenalexander@gmail.com>
2024-07-24 02:32:04 -0400

    readme change and port

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const port = 4000;
 require('dotenv').config();
+const port = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
